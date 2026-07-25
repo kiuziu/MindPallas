d5b3c246bcb27aba7920a32e7a6c5520e5ff0f1c
Manuel <kiuziu@gmail.com>
2026-07-25 21:46:17 +0200

    Register and run LeetCode tasks in App
    Added task registration and execution in main method.

--- src/main/java/com/mindpallas/core/App.java
+++ src/main/java/com/mindpallas/core/App.java
@@ -1,8 +1,25 @@
 package com.mindpallas.core;
 
+import com.mindpallas.leetcode.LeetCodeRegistry;
+import com.mindpallas.leetcode.math.MaxProductTask;
+import com.mindpallas.leetcode.strings.TokenizerTask;
+import com.mindpallas.leetcode.arrays.TwoSumTask;
+import com.mindpallas.leetcode.strings.ReverseStringTask;
+
 public class App {
     public static void main(String[] args) {
-        var engine = new MindEngine();
-        engine.run();
+
+        // Register tasks
+        LeetCodeRegistry.register(new MaxProductTask());
+        LeetCodeRegistry.register(new TokenizerTask());
+        LeetCodeRegistry.register(new TwoSumTask());
+        LeetCodeRegistry.register(new ReverseStringTask());
+
+        // List all tasks
+        LeetCodeRegistry.list();
+
+        // Run a specific task
+        System.out.println("\nRunning task: reverse-string\n");
+        LeetCodeRegistry.run("reverse-string");
     }
 }
