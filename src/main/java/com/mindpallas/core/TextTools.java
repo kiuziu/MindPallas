975d528b0264407442b4d7f1e07f5112e6fe5b49
Manuel <kiuziu@gmail.com>
2026-07-25 18:45:38 +0200

    Delete src/main/java/com/mindpallas/core/TextTools.java

--- src/main/java/com/mindpallas/core/TextTools.java
+++
@@ -1,9 +0,0 @@
-package com.mindpallas.util;
-
-public class TextTools {
-
-    public static boolean isPalindrome(String s) {
-        var clean = s.replaceAll("\\s+", "").toLowerCase();
-        return clean.contentEquals(new StringBuilder(clean).reverse());
-    }
-}
