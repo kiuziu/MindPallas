8163ce2ef8eec4741a4ffafdd03e86386cfbdd55
Manuel <kiuziu@gmail.com>
2026-07-25 18:43:17 +0200

    Add MindEngine class with run method

---
+++ src/main/java/com/mindpallas/core/MindEngine.java
@@ -0,0 +1,9 @@
+package com.mindpallas.core;
+
+public class MindEngine {
+
+    public void run() {
+        System.out.println("MindPallas – Modern Java 2026");
+        System.out.println("System ready.");
+    }
+}
