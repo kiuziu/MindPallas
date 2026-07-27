416a3012099975556096a52f66a9b23407da329d
Manuel <kiuziu@gmail.com>
2026-07-27 07:49:33 +0200

    Implement maxProduct method for maximum product

---
+++ src/main/java/com/mindpallas/leetcode/arrays/MaximumProductOfTwoElements.java
@@ -0,0 +1,16 @@
+public class MaximumProductOfTwoElements {
+    public int maxProduct(int[] nums) {
+	    int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE;
+	
+	    for (int x : nums) {
+	        if (x > max1) {
+	            max2 = max1;
+	            max1 = x;
+	        } else if (x > max2) {
+	            max2 = x;
+	        } 
+	    }
+	
+	    return (max1-1) * (max2-1);
+    }
+}
